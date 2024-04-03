59796a4c2c8f6d82ee845cc765164eafee6b3250
Shubham2110991343 <103366220+Shubham2110991343@users.noreply.github.com>
2024-04-03 13:05:22 +0530

    hello

---
+++ vscode/eli.js
@@ -0,0 +1,11 @@
+function plus(){
+    console.log("button was clicked")
+}
+
+let lap1 = 21
+let lap2 = 22
+let lap3 = 23
+function sumoflaps(){
+    console.log(lap1+lap2+lap3)
+    }
+    sumoflaps()
